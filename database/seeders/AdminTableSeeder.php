c73a99a185ebd4f303156b5053505b500318232b
unknown <mostafa0alii@gmail.com>
2024-06-30 08:34:48 -0700

    project init

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Seeders;
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\{DB, Schema};
+use Illuminate\Support\Str;
+class AdminTableSeeder extends Seeder {
+    public function run() {
+        Schema::disableForeignKeyConstraints();
+        DB::table('admins')->truncate();
+        $admin = Admin::create([
+            'name'          =>  'Mostafa',
+            'email'         =>  'admin@admin.com',
+            'password'      =>  bcrypt('123123'),
+            'remember_token'=> Str::random(10),
+            'status'        =>  'active',
+        ]);
+        env('APP_ENV') === 'local' ? Admin::factory()->count(10)->create() : $admin;
+        Schema::enableForeignKeyConstraints();
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\{App,File};
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+    public function run(): void {
+        env('APP_ENV') === 'local' ? $this->localSeeders() : $this->productionSeeder();
+    }
+
+    private function localSeeders() {
+        $this->seederCalling();
+    }
+
+    private function productionSeeder() {
+        $this->seederCalling();
+    }
+
+    private function seederCalling() {
+        $seederFiles = File::allFiles(database_path('seeders'));
+        $seederFileArray = [];
+        foreach ($seederFiles as $seederFile) {
+            if ($seederFile->getBasename() !== 'DatabaseSeeder.php') 
+                $seederFileArray[] = $seederFile;
+        }
+        usort($seederFileArray, function ($time_a, $time_b) {
+            return filectime($time_a->getRealPath()) <=> filectime($time_b->getRealPath());
+        });
+        
+        foreach ($seederFileArray as $seederFile) {
+            $seederClassNamespace = 'Database\\Seeders\\' . $seederFile->getBasename('.php');
+            if (class_exists($seederClassNamespace)) 
+                $this->call($seederClassNamespace);
+        }
+    }
+}
